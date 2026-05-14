35f8fce6d747820bb7445f9a636a4932e527ade6
BaThi-hcmus <huynhbathi2006@gmail.com>
2026-05-14 19:02:10 +0700

    [ADD] ket noi voi sql server

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. Load file .env
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // 2. Cấu hình TypeORM
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mssql',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT', '1433'), 10),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'], // đường dẫn tới các file entity định nghĩa schema
+        synchronize: true, // Tự động tạo bảng dựa trên code Entity (Chỉ dùng khi Dev)
+        extra: {
+          trustServerCertificate: true, // Quan trọng để tránh lỗi SSL trên Local
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
